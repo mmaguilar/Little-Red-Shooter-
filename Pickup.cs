78269c50cc5543351451d2e4a5e19c3878dc1f5a
Miriam Aguilar <77868811+mmaguilar@users.noreply.github.com>
2022-11-15 12:38:29 -0800

    Pickup script for Scene 1

---
+++ Pickup.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    //max health
+    public float MaxHealth;
+
+    public float CurrentHealth;
+
+    public int coins;
+
+    public float Speed;
+
+}
